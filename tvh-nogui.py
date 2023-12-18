74bb2bd8cc5f7b116fddc7003288eabaabaca4b0
aosucas499 <aosucas499@gmail.com>
2023-12-18 19:28:50 +0100

    using of omxplayer-wrapper to change channels

---
+++ tvh-nogui.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import requests
+from requests.auth import HTTPDigestAuth
+import json
+import sys
+import termios
+import tty
+from omxplayer.player import OMXPlayer
+import time
+
+debug = True
+
+# Ruta al archivo de configuración
+CONFIG_FILE_PATH = os.path.join(os.path.expanduser("~"), "tvheadend_config.json")
+
+def obtener_configuracion_tvheadend():
+    # Verificar si existe un archivo de configuración previo
+    if os.path.exists(CONFIG_FILE_PATH):
+        with open(CONFIG_FILE_PATH, "r") as file:
+            config = json.load(file)
+    else:
+        # Si no hay un archivo de configuración, solicitar la configuración al usuario
+        config = {
+            "TVHEADEND_IP": input("Ingrese la dirección IP y el puerto de TVHeadend: "),
+            "TVHEADEND_USERNAME": input("Ingrese el nombre de usuario de TVHeadend: "),
+            "TVHEADEND_PASSWORD": input("Ingrese la contraseña de TVHeadend: "),
+        }
+
+        # Guardar la configuración en el archivo
+        with open(CONFIG_FILE_PATH, "w") as file:
+            json.dump(config, file, indent=2)
+
+    return config
+
+def obtener_lista_canales(config):
+
+# Obtener la lista de canales desde TVHeadend con autenticación digest
+    url = f"http://{config['TVHEADEND_IP']}/api/channel/grid"
+    filter = {
+        "field": "number",
+        "value": 0
+    }
+
+    # Configurar los parámetros de la solicitud
+    parametros = {
+        "limit": 100,  # Limitar a 500 canales para fines de depuración
+        "meta": 0
+    }
+
+    try:
+        response = requests.get(url, auth=HTTPDigestAuth(config['TVHEADEND_USERNAME'], config['TVHEADEND_PASSWORD']), params=parametros)
+        response.raise_for_status()  # Verificar si hay errores en la respuesta
+    except requests.exceptions.RequestException as e:
+        print(f"Error en la solicitud de la API: {e}")
+        return []
+
+    if debug:
+        # Imprimir mensajes de depuración
+        print(f"Debug - URL de la solicitud: {response.url}")
+        print(f"Debug - Respuesta de la solicitud: {response.text}")
+
+    try:
+        data = response.json()
+    except json.JSONDecodeError as e:
+        print(f"Error al decodificar la respuesta JSON: {e}")
+        return []
+
+    # Obtener canales activos sin filtrar
+    canales_activos = [(channel['number'], channel['name']) for channel in data.get('entries', [])]
+    
+    if debug:
+        # Imprimir información detallada sobre los canales
+        print("Debug - Información detallada de los canales:")
+        for channel in data.get('entries', []):
+             number = channel.get('number', 'N/A')
+             name = channel.get('name', 'N/A')
+             enabled = channel.get('enabled', 'N/A')
+             print(f"Debug - Canal: {number} - {name} (Habilitado: {enabled})")
+
+    # Ordenar por el número del canal
+    canales_activos.sort(key=lambda x: x[0])
+
+    return canales_activos
+
+# Variable global para la instancia de OmxplayerWrapper
+reproductor = None
+
+def reproducir_canal(nombre_canal, config):
+    global reproductor  # Accede a la variable global
+
+    # Detener la reproducción si ya hay un reproductor en ejecución
+    if reproductor is not None:
+        reproductor.stop()
+        reproductor = None
+
+    # Obtener el número del canal a partir del nombre
+    canales = obtener_lista_canales(config)
+    for canal in canales:
+        if canal[1] == nombre_canal:
+            numero_canal = canal[0]
+            break
+
+    # Construir la URL del flujo del canal usando el número del canal y las credenciales
+    url = f"http://{config['TVHEADEND_USERNAME']}:{config['TVHEADEND_PASSWORD']}@{config['TVHEADEND_IP']}/stream/channelnumber/{numero_canal}?profile=pass"
+
+    # Crear o reutilizar la instancia de OmxplayerWrapper
+    if reproductor is None:
+        reproductor = OMXPlayer(url, dbus_name='org.mpris.MediaPlayer2.omxplayer1')
+    else:
+        reproductor.load(url)
+
+    # Imprimir mensajes de depuración
+    if debug:
+        print(f"Debug - Reproduciendo el canal: {nombre_canal}")
+
+def obtener_tecla():
+    termios.tcgetattr(sys.stdin)
+    tty.setcbreak(sys.stdin)
+    return sys.stdin.read(1)
+
+def main():
+    while True:
+        # Obtener o solicitar la configuración de TVHeadend
+        configuracion_tvheadend = obtener_configuracion_tvheadend()
+
+        # Obtener la lista de canales activos sin ordenar
+        canales = obtener_lista_canales(configuracion_tvheadend)
+
+        # Ordenar la lista de canales por el número del canal
+        canales.sort(key=lambda x: x[0])
+
+        # Imprimir la lista de canales
+        print("Canales disponibles:")
+        for i, (numero, canal) in enumerate(canales, start=1):
+            print(f"{i}. {numero}: {canal}")
+
+        # Solicitar al usuario que elija un canal sin pulsar "Enter"
+        print("Seleccione el número del canal que desea ver (0 para salir): ", end='', flush=True)
+
+        seleccion = ''
+        while seleccion == '':
+            seleccion = obtener_tecla()
+
+        # Verificar si el usuario quiere salir
+        if seleccion == '0':
+            print("\nSaliendo del programa.")
+            break
+
+        try:
+            seleccion = int(seleccion) - 1
+
+            # Verificar si la selección es válida
+            if 0 <= seleccion < len(canales):
+                canal_seleccionado = canales[seleccion][1]
+                print(f"Reproduciendo el canal: {canal_seleccionado}")
+
+                # Reproducir el canal seleccionado
+                reproducir_canal(canal_seleccionado, configuracion_tvheadend)
+            else:
+                print("Selección inválida.")
+        except ValueError:
+            print("Entrada no válida.")
+
+if __name__ == "__main__":
+    main()
+
